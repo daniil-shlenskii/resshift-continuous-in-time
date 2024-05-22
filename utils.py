85bdf79330391963e6aee96a239df425fb32777d
Daniil Shlyonskiy <d-shlenskii@terranova.vla.yp-c.yandex.net>
2024-05-22 15:15:07 +0300

    utils: prepare model

--- utils.py
+++ utils.py
@@ -5,25 +5,30 @@ import importlib
 TARGET_KEY = "target"
 PARAMS_KEY = "params"
 CKPT_PATH_KEY = "ckpt_path"
-DTYPE_KEY = "dtype"
+USE_FP_16_KEY = "use_fp16"
+
+def prepare_model(obj, config):
+    if CKPT_PATH_KEY in config:
+        sd = torch.load(config[CKPT_PATH_KEY])
+        filter_prefix = config.get("filter_prefix")
+        if filter_prefix:
+            sd = {k[len(filter_prefix):]: v for k, v in sd.items() if k.startswith(filter_prefix)}
+        obj.load_state_dict(sd)
+
+    if USE_FP_16_KEY in config:
+        half = config[USE_FP_16_KEY]
+        if half:
+            obj.to(torch.float16)
+        else:
+            obj.to(torch.float32)
+
+    return obj
 
 def instantiate_from_config(config):
     target = config[TARGET_KEY]
     params = config[PARAMS_KEY]
     obj =  get_obj_from_str(target)
     obj = obj(**params)
-
-    if CKPT_PATH_KEY in config:
-        sd = torch.load(config[CKPT_PATH_KEY])
-        obj.load_state_dict(sd)
-
-    if DTYPE_KEY in config:
-        half = config[DTYPE_KEY]
-        if half:
-            obj.to(torch.float16)
-        else:
-            obj.to(torch.float32)
-    
     return obj
 
 def get_obj_from_str(dot_separated_names):
